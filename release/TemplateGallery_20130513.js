9a1dde50efbeabcc99f93ae822809cc2017dbf09
unknown <Fahim@Fahim-PC.(none)>
2013-05-13 21:27:58 +0100

    release

--- release/TemplateGallery_20130513.js
+++ release/TemplateGallery_20130513.js
@@ -95,25 +95,12 @@ var Class={_super:function(e,t){e.baseConstructor[t].call(e)},extend:function(e,
 			li = document.createElement("li");
 			li.className="galleryLI";
 			li.style.backgroundColor=Model.data.template[a].color[0];
-			li.setAttribute("templateid",a);
-			li.onclick = function(event)
-			{
-				var t = event.srcElement||event.target;
-				onClick(t.getAttribute("templateid"));
-			};
 			var img = new Image();
 			img.src= Model.data.template[a].thumb;
-			img.setAttribute("templateid",a);
 			li.appendChild(img);
 			ul.appendChild(li);
 		}
 		holder.appendChild(ul);
-	}
-	function onClick(index)
-	{
-		var url = window.LINK_URL?window.LINK_URL:"gallery.html";
-		 var win=window.open(url+"?templateid="+index, window.LINK_TARGET?window.LINK_TARGET:'_blank');
-  		win.focus();
 	}
 	function onResize() {
 
